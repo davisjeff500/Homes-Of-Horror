fd8856ed0118751d63bda9640ab9df2aed6da8c0
MoonstruckOccultist <justgive1234@gmail.com>
2023-11-24 13:39:23 -0700

    Added tailwind and basic start to website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './assets/js/script.js',
+    './**/*.{html,js}',
+    './assets/js/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
